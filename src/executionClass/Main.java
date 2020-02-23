7ed1912ebe844567eb04c9ff3faf3d01fb56dfef
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 00:50:38 +0530

    tell-->object of Scanner class.

--- src/executionClass/Main.java
+++ src/executionClass/Main.java
@@ -3,9 +3,13 @@ package executionClass;
 import Client.Client;
 import LinkList.LinkedListDefinition;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         boolean turnoff = false;
         LinkedListDefinition<Client> listContact=new LinkedListDefinition();
+        Scanner tell=new Scanner(System.in);
+
     }
 }
