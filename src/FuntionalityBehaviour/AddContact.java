767142de89e7d6981c76e6c214d227a034437318
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:09:32 +0530

    tell-->object of Scanner class

--- src/FuntionalityBehaviour/AddContact.java
+++ src/FuntionalityBehaviour/AddContact.java
@@ -2,6 +2,10 @@ package FuntionalityBehaviour;
 
 import Client.Client;
 
+import java.util.Scanner;
+
 public class AddContact {
     Client client = new Client();
+    Scanner tell = new Scanner(System.in);
+
 }
