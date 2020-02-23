348649d72b45c9a7b95cd65e680a6d2fd0af5bec
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:36:14 +0530

    tell--->object of Scanner Class.

--- src/FuntionalityBehaviour/DeleteContact.java
+++ src/FuntionalityBehaviour/DeleteContact.java
@@ -2,9 +2,11 @@ package FuntionalityBehaviour;
 
 import LinkList.LinkedListDefinition;
 
+import java.util.Scanner;
+
 public class DeleteContact {
     public void removeContactFromList(LinkedListDefinition person) {
-
+        Scanner tell = new Scanner(System.in);
     }
 
 }
