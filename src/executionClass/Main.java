9bf30abd2ae7e4d446b906c0c64d43003f4b176c
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 00:53:37 +0530

    this will be all the available functions.i

--- src/executionClass/Main.java
+++ src/executionClass/Main.java
@@ -12,7 +12,11 @@ public class Main {
         Scanner tell=new Scanner(System.in);
         System.out.println("Welcome to Abhi Contact list \uD83D\uDE01:");
         while (!turnoff) {
-
+            System.out.println("Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
 
 
 
