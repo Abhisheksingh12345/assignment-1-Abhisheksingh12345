7fcdc17355d831770890a6031c920869a433ff09
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:38:32 +0530

    view the contact list and delte for serail no.

--- src/FuntionalityBehaviour/DeleteContact.java
+++ src/FuntionalityBehaviour/DeleteContact.java
@@ -1,5 +1,6 @@
 package FuntionalityBehaviour;
 
+import Client.Client;
 import LinkList.LinkedListDefinition;
 
 import java.util.Scanner;
@@ -7,6 +8,13 @@ import java.util.Scanner;
 public class DeleteContact {
     public void removeContactFromList(LinkedListDefinition person) {
         Scanner tell = new Scanner(System.in);
+        System.out.println("Here are all your contacts:");
+        for (int i = 0; i < person.getSize(); i++) {
+            Client person1 = (Client) person.dataAtIndex(i);
+            System.out.println(i + 1 + ". " + person1.getFirstName() + " " + person1.getLastName());
+        }
+
+
     }
 
 }
