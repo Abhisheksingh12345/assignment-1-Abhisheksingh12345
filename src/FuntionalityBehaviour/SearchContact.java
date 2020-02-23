20880cd65d7ffa7f81df3fa28765e913dcac0274
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:44:33 +0530

    tell-->object of Scanner Class.

--- src/FuntionalityBehaviour/SearchContact.java
+++ src/FuntionalityBehaviour/SearchContact.java
@@ -3,11 +3,12 @@ package FuntionalityBehaviour;
 import LinkList.LinkedListDefinition;
 
 import javax.swing.text.View;
+import java.util.Scanner;
 
 public class SearchContact {
     public void seach(LinkedListDefinition person) {
         View view = new View();
-
+        Scanner tell = new Scanner(System.in);
     }
 
 
