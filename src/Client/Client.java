17fc0e7c697125e7ed2827d50d795548ee8380f6
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 00:41:16 +0530

    these are the getter of all the field fielf-->firstname,lastname,email,contactno.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -6,4 +6,19 @@ public class Client {
     private LinkedListDefinition<Long> contactNo;
     private String emailAddress;
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setContactNo(LinkedListDefinition<Long> contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
