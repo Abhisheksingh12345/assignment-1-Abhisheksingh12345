75d1bc218be4722f671835dafe8af7dbc805256b
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:40:25 +0530

    detele the contact form th list.

--- src/FuntionalityBehaviour/DeleteContact.java
+++ src/FuntionalityBehaviour/DeleteContact.java
@@ -13,6 +13,11 @@ public class DeleteContact {
             Client person1 = (Client) person.dataAtIndex(i);
             System.out.println(i + 1 + ". " + person1.getFirstName() + " " + person1.getLastName());
         }
+        System.out.println("Press the number against the contact to delete it: ");
+        int choice = Integer.parseInt(tell.nextLine());
+        Client person2 = (Client) person.dataAtIndex(choice - 1);
+        System.out.println(person2.getFirstName() + "'s contact deleted from list!");
+        person.remove(choice - 1);
 
 
     }
