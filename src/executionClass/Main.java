88779be3b8fafd262704a5962c9fa9ce51f31a48
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 00:49:41 +0530

    listContaact-->object of linklistDefinition class of Client type(genric type) with help of which we store the referance in client class in the node.

--- src/executionClass/Main.java
+++ src/executionClass/Main.java
@@ -1,7 +1,11 @@
 package executionClass;
 
+import Client.Client;
+import LinkList.LinkedListDefinition;
+
 public class Main {
     public static void main(String[] args) {
         boolean turnoff = false;
+        LinkedListDefinition<Client> listContact=new LinkedListDefinition();
     }
 }
