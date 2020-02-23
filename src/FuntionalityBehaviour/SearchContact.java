0e671cb6bd845ccf07c5f0ee0bed454f62990020
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:44:03 +0530

    view-->object of view Class.

--- src/FuntionalityBehaviour/SearchContact.java
+++ src/FuntionalityBehaviour/SearchContact.java
@@ -2,9 +2,11 @@ package FuntionalityBehaviour;
 
 import LinkList.LinkedListDefinition;
 
+import javax.swing.text.View;
+
 public class SearchContact {
     public void seach(LinkedListDefinition person) {
-
+        View view = new View();
 
     }
 
