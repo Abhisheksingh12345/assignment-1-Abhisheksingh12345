2fb04361726e5574919722473752463e69df2c5e
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 01:25:10 +0530

    this will  be back the control to the while loop.

--- src/FuntionalityBehaviour/AddContact.java
+++ src/FuntionalityBehaviour/AddContact.java
@@ -15,11 +15,32 @@ public class AddContact {
         client.setFirstName(tell.nextLine());
         System.out.print("Last Name : ");
         client.setLastName(tell.nextLine());
-        LinkedListDefinition<Long> contactNumber=new LinkedListDefinition<Long>();
-        while (finish!=true){
+        LinkedListDefinition<Long> contactNumber = new LinkedListDefinition<Long>();
+        while (finish != true) {
             System.out.print("Contact number: ");
             contactNumber.add(Long.parseLong(tell.nextLine()));
-        } System.out.print("Would you like to add another contact number? (y/n): ");
-        char choise = (tell.nextLine()).charAt(0);
+            System.out.print("Would you like to add another contact number? (y/n): ");
+            char choise = (tell.nextLine()).charAt(0);
+            if (choise!= 'n') {
+
+            } else
+                finish = true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
-}
