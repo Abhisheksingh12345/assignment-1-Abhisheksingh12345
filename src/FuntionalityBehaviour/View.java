a21281ba49a61a46067068f5eed443418af151b3
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 08:25:22 +0530

    call the printContact function

--- src/FuntionalityBehaviour/View.java
+++ src/FuntionalityBehaviour/View.java
@@ -2,6 +2,9 @@ package FuntionalityBehaviour;
 
 public class View {
     public boolean viewList(LinkedListDefinitionClass person) {
-
+        System.out.println("--------------Here are all your Contacts---------");
+        int size = person.getSize();
+        printContact(person, size);
+        return true;
     }
 }
