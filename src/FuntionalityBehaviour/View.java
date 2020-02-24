98d9c11e13b19d9a3525046070b1d44a12af7d07
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 08:26:26 +0530

    to holl list

--- src/FuntionalityBehaviour/View.java
+++ src/FuntionalityBehaviour/View.java
@@ -1,5 +1,7 @@
 package FuntionalityBehaviour;
 
+import Client.Client;
+
 public class View {
     public boolean viewList(LinkedListDefinitionClass person) {
         System.out.println("--------------Here are all your Contacts---------");
@@ -7,4 +9,15 @@ public class View {
         printContact(person, size);
         return true;
     }
+
+    public void printContact(LinkedListDefinitionClass<Client> totalContact, int size) {
+        for (int i = 0; i < size; i++) {
+
+
+        }
+
+
+    }
+
+
 }
