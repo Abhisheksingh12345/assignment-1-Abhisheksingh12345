f528b1cdacde7db345bce0e8cf340d0c052d857c
Mokey-D-luffy88 <atching123@gmail.com>
2020-02-24 08:27:21 +0530

    view contacts

--- src/FuntionalityBehaviour/View.java
+++ src/FuntionalityBehaviour/View.java
@@ -12,7 +12,18 @@ public class View {
 
     public void printContact(LinkedListDefinitionClass<Client> totalContact, int size) {
         for (int i = 0; i < size; i++) {
-
+            Client person = totalContact.dataAtIndex(i);
+            System.out.println("------*------------*---------**--------\n" +
+                    "First Name :" + person.getFrsitName() + "\n" +
+                    "Last Name :" + person.getLastName());
+            if (person.getContactNo().getSize() > 1) {
+                System.out.println("Contact Number(S): ");
+                for (int j = 0; j < person.getContactNo().getSize(); j++) {
+                    System.out.print(person.getContactNo().dataAtIndex(i) + (j < person.getContactNo().getSize() - 1 ? "," : ""));
+                }
+            } else {
+                System.out.print("Contact Number :" + person.getContactNo().dataAtIndex(0));
+            }
 
         }
 
